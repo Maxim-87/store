090ff75b521bddeeaa84ff97fbf2359600725fa5
Maxim87 <maksboks87@mail.ru>
2022-03-24 14:18:45 +0700

    add new components

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const sequelize = require('./db') //step 12
-const models = require('./models/models')
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db'); //step 12
+const models = require('./models/models');
+const cors = require('cors');
+const fileUpload = require('express-fileupload')
+const router = require('./routes/index');
+const errorHandler = require('../server/middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000;
 
 const app = express()
+app.use(cors())
+app.use(express.json()) // чтобы парсило JSON формат
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+app.use(errorHandler)
 
 const start = async () => {
         try {
